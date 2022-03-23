8488b769d1d9873718971fb7dd937a4cb97eeceb
KyoKiriteri <sara.a.loven@gmail.com>
2022-03-23 08:41:40 +0200

    Initial Commit
    .gitignore adjustment and repo addition to project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Controller values")]
+    [SerializeField] private CharacterController playerCtrlr;
+    [Space(10)]
+    [Tooltip("Modify the base speed of the player")]
+    [SerializeField] private float speed = 0.0f;
+    [Tooltip("Modify the 'sprint' modifier effect")]
+    [SerializeField] private float sprint = 1.0f;
+
+    private void Awake()
+    {
+        playerCtrlr.detectCollisions = false;
+    }
+
+    void Update()
+    {
+        PlayerMovementVariable();
+    }
+
+    private void PlayerMovementVariable()
+    {
+        float horizontalMvmt = Input.GetAxisRaw("Horizontal");
+        float verticalMvmt = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey("left shift"))
+        {
+            sprint = 2.0f;
+        }
+        else
+        {
+            sprint = 1.0f;
+        }
+
+        // Calculates the 'move' parameter for the movement size and direction
+        Vector3 move = transform.forward * verticalMvmt + transform.right * horizontalMvmt;
+
+        // Calculates together the base movement, speed, sprint modifier and time, making movement smooth
+        playerCtrlr.Move(speed * sprint * Time.deltaTime * move);
+    }
+}
